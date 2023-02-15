80f62993570fc0c7d6c33467c9e2666a5abf5692
htetsoesan <htetsan2580@gmail.com>
2023-02-15 16:55:46 +0630

    Initial commit

---
+++ app/src/main/java/com/dev_hss/firebasechatapp/MainActivity.kt
@@ -0,0 +1,138 @@
+package com.dev_hss.firebasechatapp
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dev_hss.firebasechatapp.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var manager: LinearLayoutManager
+
+    // Firebase instance variables
+    private lateinit var auth: FirebaseAuth
+
+    private val openDocument = registerForActivityResult(MyOpenDocumentContract()) { uri ->
+        uri?.let { onImageSelected(it) }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // When running in debug mode, connect to the Firebase Emulator Suite.
+        // "10.0.2.2" is a special IP address which allows the Android Emulator
+        // to connect to "localhost" on the host computer. The port values (9xxx)
+        // must match the values defined in the firebase.json file.
+//        if (BuildConfig.DEBUG) {
+//            Firebase.database.useEmulator("10.0.2.2", 9000)
+//            Firebase.auth.useEmulator("10.0.2.2", 9099)
+//            Firebase.storage.useEmulator("10.0.2.2", 9199)
+//        }
+
+        // Initialize Firebase Auth and check if the user is signed in
+        auth = Firebase.auth
+        if (auth.currentUser != null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(SignInActivity.newIntent(this))
+            finish()
+            return
+        }
+
+        // Initialize Realtime Database and FirebaseRecyclerAdapter
+        // TODO: implement
+
+        // Disable the send button when there's no text in the input field
+        // See MyButtonObserver for details
+        binding.messageEditText.addTextChangedListener(MyButtonObserver(binding.sendButton))
+
+        // When the send button is clicked, send a text message
+        // TODO: implement
+
+        // When the image button is clicked, launch the image picker
+        binding.addMessageImageView.setOnClickListener {
+            openDocument.launch(arrayOf("image/*"))
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in.
+        if (auth.currentUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(SignInActivity.newIntent(this))
+            finish()
+            return
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.sign_out_menu -> {
+                signOut()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun onImageSelected(uri: Uri) {
+        // TODO: implement
+    }
+
+    private fun putImageInStorage(storageReference: StorageReference, uri: Uri, key: String?) {
+        // Upload the image to Cloud Storage
+        // TODO: implement
+    }
+
+    private fun signOut() {
+        AuthUI.getInstance().signOut(this)
+        SignInActivity.newIntent(this)
+        finish()
+    }
+
+    private fun getPhotoUrl(): String? {
+        val user = auth.currentUser
+        return user?.photoUrl?.toString()
+    }
+
+    private fun getUserName(): String? {
+        val user = auth.currentUser
+        return if (user != null) {
+            user.displayName
+        } else ANONYMOUS
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        const val MESSAGES_CHILD = "messages"
+        const val ANONYMOUS = "anonymous"
+        private const val LOADING_IMAGE_URL = "https://www.google.com/images/spin-32.gif"
+    }
+}
